10b0e331b0d3f53b46a72b6d5f0256091b9566b1
ericmutton <64441308+ericmutton@users.noreply.github.com>
2019-01-20 21:43:48 -0600

    Revert "Update onceler.py"
    This reverts commit 274956260fd7070752e7829b293a794e950767de.

--- onceler.py
+++ onceler.py
@@ -2,7 +2,6 @@ import discord
 import asyncio
 import json
 
-#read and load configuration file data
 with open("config.json") as config:
     data = json.loads(config.read())
     token = data["token"]
@@ -17,36 +16,33 @@ async def on_ready():
 async def on_guild_join(joined_guild):
     reason = "to enable once-ler's #once channel feature"
     seed_role = discord.utils.get(joined_guild.roles, name="Truffula Seed")
-    if seed_role is None: #check for lack of "Truffula Seed" role
-        seed_role = await joined_guild.create_role(name="Truffula Seed", reason=reason) #create "Truffula Seed" role
-        await seed_role.edit(position=1) #set precidence of "Truffula Seed" role to be directly above "@everyone" role
-        #add "Truffula Seed" role to every current member
+    if seed_role is None:
+        seed_role = await joined_guild.create_role(name="Truffula Seed", reason=reason)
+        await seed_role.edit(position=1)
         for member in joined_guild.members:
             await member.add_roles(seed_role)
             print(f"the Once-ler gave {member} one truffula seed")
-    default_role = joined_guild.default_role #"@everyone" role
+    default_role = joined_guild.default_role
     overwrites = {
         default_role: discord.PermissionOverwrite(send_messages=False),
         seed_role: discord.PermissionOverwrite(send_messages=True)
         }
-    if discord.utils.get(joined_guild.channels, name="once") is None: #check for lack of "#once" text channel
-        await joined_guild.create_text_channel(name="once", overwrites=overwrites, reason=reason) #create "#once" text channel
+    if discord.utils.get(joined_guild.channels, name="once") is None:
+        await joined_guild.create_text_channel(name="once", overwrites=overwrites, reason=reason)
 
 @onceler.event
 async def on_member_join(member):
-    #add Truffula Seed role to every new member
     role = discord.utils.get(member.guild.roles, name="Truffula Seed")
-    if role is not None: #check for existence of "Truffula Seed" role
+    if role is not None:
         await member.add_roles(role)
         print(f"the Once-ler gave {member} one truffula seed")
 
 @onceler.event
 async def on_message(message):
-    #remove Truffula Seed role from any member who writes in "#once"
     if message.channel.name == "once":
         author = message.author
         role = discord.utils.get(author.guild.roles, name="Truffula Seed")
-        if role is not None: #check for existence of "Truffula Seed" role
+        if role is not None:
             await author.remove_roles(role)
             print(f"{author} planted their truffula seed")
 
